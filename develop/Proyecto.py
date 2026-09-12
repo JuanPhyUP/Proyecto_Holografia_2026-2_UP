91ad2ae1f9f28c48db575db4881e979f003f348e
JuanPhyUP <juanccelislozada13@gmail.com>
2026-09-12 16:03:46 -0500

    Codigo de la interfaz y multimedia necesaria para su ejecucion

---
+++ develop/Proyecto.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("HDFresnel_GOM")
+ventana.geometry("1200x600")
+ventana.resizable(False,False)
+
+barra_menu = tk.Menu(ventana)
+
+menu_archivo = tk.Menu(barra_menu, tearoff=0)
+menu_archivo.add_command(label="Abrir")
+menu_archivo.add_command(label="Guardar")
+menu_archivo.add_separator()
+menu_archivo.add_command(label="Salir", command=ventana.destroy)
+
+barra_menu.add_cascade(label="Archivo", menu=menu_archivo)
+
+menu_herramientas = tk.Menu(barra_menu, tearoff=0)
+menu_herramientas.add_command(label="Configuración")
+
+barra_menu.add_cascade(
+    label="Herramientas",
+    menu=menu_herramientas
+)
+
+menu_ayuda = tk.Menu(barra_menu,tearoff=0)
+menu_ayuda.add_command(label="Acerca de")
+
+barra_menu.add_cascade(
+    label= "Ayuda",
+    menu=menu_ayuda
+)
+
+ventana.config(menu=barra_menu)
+
+barra_herramientas = tk.Frame(
+    ventana,
+    bg='#eeeeee',
+    height=35,
+)
+
+barra_herramientas.grid(
+    row=0,
+    column=0,
+    sticky="eew"
+)
+
+tk.Button(
+    barra_herramientas,
+    text="📁"
+).pack(side="left", padx=3)
+
+tk.Button(
+    barra_herramientas,
+    text="💾"
+).pack(side="left", padx=3)
+
+tk.Button(
+    barra_herramientas,
+    text="🔍"
+).pack(side="left",padx=3)
+
+contenido = tk.Frame(ventana)
+
+contenido.grid(
+    row=1,
+    column=0,
+    padx=10,
+    pady=10
+)
+
+ventana.rowconfigure(1,weight=1)
+ventana.columnconfigure(0,weight=1)
+
+contenido.rowconfigure(0,weight=1)
+
+
+contenido.columnconfigure(0,weight=1)
+contenido.columnconfigure(1,weight=0)
+
+
+panel_izquierdo = tk.Frame(
+    contenido,
+    relief = "sunken",
+    borderwidth=1
+)
+
+panel_izquierdo.grid(
+    row=0,
+    column=0,
+    sticky="nsew",
+    padx=(0,10)
+)
+
+panel_izquierdo.rowconfigure(0,weight=1)
+panel_izquierdo.rowconfigure(1, weight=1)
+panel_izquierdo.columnconfigure(0, weight=1)
+panel_izquierdo.columnconfigure(1, weight=1)
+
+# Cuadro 1
+
+camara_frame = tk.LabelFrame(
+    panel_izquierdo,
+    text="CÁMARA"
+)
+
+camara_frame.grid(
+    row=0,
+    column=0,sticky="nsew",
+    padx=5,
+    pady=5
+)
+
+camara_frame.rowconfigure(0, weight=1)
+camara_frame.columnconfigure(0, weight=1)
+
+image_pil1 = Image.open("profile.jpg")
+image_pil1=image_pil1.resize((300,200))
+image_tk1 = ImageTk.PhotoImage(image_pil1)
+
+camara = tk.Label(
+    camara_frame,
+    image=image_tk1
+)
+
+camara.grid(
+    row=0,
+    column=0,
+    sticky="nsew",
+    padx=10,
+    pady=10
+)
+
+
+
+# Cuadro 2
+
+holograma_frame = tk.LabelFrame(
+    panel_izquierdo,
+    text="HOLOGRAMA"
+)
+
+holograma_frame.grid(
+    row=0,
+    column=1,
+    sticky="nsew",
+    padx=5,
+    pady=5
+)
+
+holograma_frame.rowconfigure(0,weight=1)
+holograma_frame.columnconfigure(0,weight=1)
+
+image_pil2 = Image.open("Holograma.bmp")
+image_pil2 = image_pil2.resize((300,200))
+image_tk2 = ImageTk.PhotoImage(image_pil2)
+
+holograma= tk.Label(
+    holograma_frame,
+    image = image_tk2
+    
+)
+
+holograma.grid(
+    row=0,
+    column=0,
+    sticky="nsew",
+    padx=10,
+    pady=10
+)
+
+# Cuadro 3
+
+reconstruccion_frame = tk.LabelFrame(
+    panel_izquierdo,
+    text="RECONSTRUCCIÓN"
+)
+
+reconstruccion_frame.grid(
+    row=1,
+    column=0,
+    sticky="nsew",
+    padx=5,
+    pady=5
+)
+
+reconstruccion_frame.rowconfigure(0,weight=1)
+reconstruccion_frame.columnconfigure(0,weight=1)
+
+image_pil3 = Image.open("Transformada_de_Fresnel.bmp")
+image_pil3 = image_pil3.resize((300,200))
+image_tk3 = ImageTk.PhotoImage(image_pil3)
+
+reconstruccion = tk.Label(
+    reconstruccion_frame,
+    image = image_tk3
+)
+
+reconstruccion.grid(
+    row=0,
+    column=0,
+    sticky="nsew",
+    padx=10,
+    pady=10
+)
+
+# Cuadro 4
+
+filtrado_frame= tk.LabelFrame(
+    panel_izquierdo,
+    text="RECONSTRUCCIÓN FILTRADA"
+)
+
+filtrado_frame.grid(
+    row=1,
+    column=1,
+    sticky="nsew",
+    padx=5,
+    pady=5
+)
+
+filtrado_frame.rowconfigure(0,weight=1)
+filtrado_frame.columnconfigure(0,weight=1)
+
+image_pil4 = Image.open("Transformada_de_Fresnel_Filtrado.bmp")
+image_pil4 = image_pil4.resize((300,200))
+image_tk4 = ImageTk.PhotoImage(image_pil4)
+
+filtrado = tk.Label(
+    filtrado_frame,
+    image = image_tk4
+)
+
+filtrado.grid(
+    row=0,
+    column=0,
+    sticky="nsew",
+    padx=10,
+    pady=10
+)
+
+ventana.mainloop()
